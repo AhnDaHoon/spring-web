6b0879b06f7245161cb54898c40da3fb5a14d96c
DH <psycn@naver.com>
2022-11-20 22:51:11 +0900

    어노테이션, 설정 변경

--- src/test/java/com/dh/admin/web/HelloControllerTest.java
+++ src/test/java/com/dh/admin/web/HelloControllerTest.java
@@ -1,9 +1,12 @@
 package com.dh.admin.web;
 
+import com.dh.admin.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,7 +18,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloContorller.class)
+@WebMvcTest(controllers = HelloContorller.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
